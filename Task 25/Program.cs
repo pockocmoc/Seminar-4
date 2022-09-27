9f68638f5742f180361c85ef3e28237139dff070
pockocmoc <pockocmoc@inbox.ru>
2022-09-27 11:07:15 +0300

    Initial commit Homework Seminar 4

---
+++ Task 25/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+   Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+    и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+int num1 = ReadData("Введите число А: ");
+int num2 = ReadData("Введите число B: ");
+
+string LineNum(int i, int p)
+{
+    string outLine = "";
+    outLine = outLine + Math.Pow(i, p);
+    return outLine;
+}
+
+string degree = LineNum(num1, num2);
+
+PrintData("Число A в натуральной степени B равно: ", degree);
+
---
+++ Task 27/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+  Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+int num = ReadData("Введите число: ");
+
+int FindSum(int sum)
+{
+    sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+int numSum = FindSum(num);
+
+PrintData("Сумма цифр числа равна: ", numSum.ToString());
